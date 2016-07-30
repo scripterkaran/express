916d0c6cf7a12b4be5baf66ec01656ba5355c338
Karan Kumar <karankumar.world@gmail.com>
2016-07-31 02:36:56 +0530

    adding new blogs and query single blog by id

--- routes/blog.js
+++ routes/blog.js
@@ -25,8 +25,63 @@ router.get('/', function(req, res, next) {
     });
 });
 
+
 router.get('/new', function(req, res) {
+    console.log('req', req )
     res.render('newblog', { title: 'New Blog' });
 });
 
-module.exports = router
+
+router.post('/new', function(req, res) {
+    // Get values from POST request. These can be done through forms or REST calls. These rely on the "name" attributes for forms
+    var title = req.body.title;
+    var content = req.body.content;
+    //call the create function for our database
+    Blog.create({
+        title : title,
+        content: content
+    }, function (err, blog) {
+          if (err) {
+              res.send("There was a problem adding the information to the database.", err);
+          } else {
+              //User has been created
+              console.log('POST creating new Blog: ' + blog);
+              res.format({
+                  //HTML response will set the location and redirect back to the home page. You could also create a 'success' page if that's your thing
+                html: function(){
+                    // If it worked, set the header so the address bar doesn't still say /adduser
+                    res.location("blog");
+                    // And forward to success page
+                    res.redirect("/blog");
+                },
+                //JSON response will show the newly created user
+                json: function(){
+                    res.json(blog);
+                }
+            });
+          }
+    })
+});
+
+router.get('/:id', function(req, res, next) {
+    Blog.findById({_id: req.params.id }, function (err, blog) {
+          if (err) {
+              return console.error(err);
+          } else {
+              //respond to both HTML and JSON. JSON responses require 'Accept: application/json;' in the Request Header
+              res.format({
+                  //HTML response will render the userlist.jade file in the views folder. We are also setting "userlist" to be an accessible variable in our jade view
+                html: function(){
+                    res.render('blogdetail', {
+                          "blogdetail" : blog
+                      });
+                },
+                //JSON response will show all users in JSON format
+                json: function(){
+                    res.json(blog);
+                }
+            });
+          }
+    });
+});
+module.exports = router;
